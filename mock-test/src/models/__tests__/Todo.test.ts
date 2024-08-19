b61e41f12b16023f7e8e90e56983a9293e5ff90b
dpclfk <lee@iseungbaeui-MacBookAir.local>
2024-08-19 13:52:06 +0900

    fix: test env

--- mock-test/src/models/__tests__/Todo.test.ts
+++ mock-test/src/models/__tests__/Todo.test.ts
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.join(__dirname, ".env") });
+dotenv.config();
 
 import Todo from "../Todo";
 import sequelize from "../database";
